02d7ec19b6ecb95f0e218e17fd3b4241fe9abded
David DeWinter <davidewi@microsoft.com>
2016-10-10 09:10:00 -0700

    Initial commit

---
+++ systemjs.config.js
@@ -0,0 +1,12 @@
+(function (global) {
+    var config = {
+        paths: {
+            'npm:': 'node_modules/'
+        },
+        map: {
+
+        }
+    };
+
+    System.config(config);
+})(this);
