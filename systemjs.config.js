f3de5afd07e5b4c06f931573009f0155cfd3a358
David DeWinter <david.dewinter@gmail.com>
2016-11-16 17:36:36 -0800

    Comments for systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,10 +1,19 @@
 (function (global) {
     var config = {
         paths: {
+            // paths serve as alias
             'npm:': 'node_modules/'
         },
+        // map tells the System loader where to look for things
         map: {
 
+        },
+        // packages tells the System loader how to load when no filename and/or no extension
+        packages: {
+            // rxjs: {
+            //     main: './main.js',
+            //     defaultExtension: 'js'
+            // }
         }
     };
 
